c23dd4b5cdd6fc66bc1e04f71a58b6441b3830de
Odranreb10 <bernardotaylor10@gmail.com>
2026-09-18 16:43:22 -0300

    Funcionando

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once __DIR__ . '/functions/server.php';
-require_once __DIR__ . '/router/router.php';
-require_once __DIR__ . '/middleware/middleware.php';
-require_once __DIR__ . '/dispatcher/dispatcher.php';
-require_once __DIR__ . '/controllers/usuario_controller.php';
-require_once __DIR__ . '/services/usuario_service.php';
+include __DIR__ . '/main.php';
+servidorHttp();
+?>
